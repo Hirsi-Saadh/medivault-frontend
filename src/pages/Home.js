7e1b42a347ed7c9791f561285870c275248e4592
itz_me_saadh <itzmesaadh@gmail.com>
2023-07-25 20:38:08 +0530

    Succesful DB Connection

---
+++ src/pages/Home.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios";
+
+export default function Home() {
+
+    const [users,setUsers]=useState([])
+
+    useEffect(()=>{
+        loadUsers();
+    },[]);
+
+    const loadUsers=async()=>{
+        const result=await axios.get("http://localhost:8080/users")
+        setUsers(result.data)
+    }
+
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Username</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    {
+        users.map((user, index)=>(
+            <tr>
+            <th scope="row" key={index}>{index+1}</th>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>
+                <button className="btn btn-primary mx-2"></button>
+                <button className="btn btn-primary mx-2"></button>
+                <button className="btn btn-primary mx-2"></button>
+            </td>
+            </tr>
+        ))
+    }
+
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
