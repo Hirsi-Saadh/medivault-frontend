05d8625d734b9a893df8df0bc730b22f995ac4cb
itz_me_saadh <itzmesaadh@gmail.com>
2023-09-11 22:37:04 +0530

    dashboard path set

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,44 @@
-import React, { useEffect } from 'react';
-import { useAuth } from '../firebase'; // Import the useAuth hook from your firebase.js file
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../firebase';
+import {useUserData} from "../users/userUtils";
 
 const Home = () => {
     const user = useAuth(); // Use the useAuth hook to get the authenticated user
+    const navigate = useNavigate();
+    const { userType } = useUserData(user);
 
+    // Check if the user is authenticated
+    if (user) {
+        // If authenticated, navigate to the dashboard
+        switch (userType) {
+            case 'patient'||'PATIENT':
+                navigate(`/patient/dashboard`);
+                break;
+            case 'doctor'||'DOCTOR':
+                navigate('/doctor');
+                break;
+            case 'HOSPITAL':
+                navigate(`/hospital/dashboard`);
+                break;
+            case 'PHARMACY':
+                navigate('/pharmacy/pharmacy');
+                break;
+            case 'LABORATORY':
+                navigate('/laboratory/dashboard');
+                break;
+            default:
+                // Default redirect if user type is not recognized
+                navigate('/');
+                break;
+        }
+    }
 
+    // If not authenticated or after the redirect, stay on the home page
     return (
-            <div>
-
-            </div>
+        <div>
+            {/* Your home page content */}
+        </div>
     );
 };
 
---
+++ src/patient/patientDashboard.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useAuth } from '../firebase'; // Import the useAuth hook from your firebase.js file
+import { usePatientData } from './patientUtils';
+import Sidepane from '../components/layout/sidepane';
+
+export default function PatientDashboard() {
+    const patient = useAuth(); // Use the useAuth hook to get the authenticated user
+    const { userType, email, username, FirstName, LastName, Age, Address, DateOfBirth } = usePatientData(patient);
+
+    useEffect(() => {
+        // You can use the userType, email, and username here
+        console.log('user type:', userType);
+        console.log('username:', username);
+        console.log('email:', email)
+        console.log('First Name:', FirstName);
+        console.log('Last Name:', LastName);
+        console.log('Age:', Age);
+        console.log('Address:', Address);
+        console.log('DOB:', DateOfBirth);
+    }, [userType, email, username, FirstName, LastName, Age, Address, DateOfBirth]);
+
+    return (
+        <div className="d-flex" style={{maxHeight: '80vh'}}>
+            <Sidepane />
+            <div className="container" >
+
+            </div>
+        </div>
+    );
+}
